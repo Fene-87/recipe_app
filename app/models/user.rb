99c71ee89b24d4c5ee308186e361c7686b3524c1
Fene-87 <fenekayas87@gmail.com>
2023-05-22 22:07:48 +0300

    Generate User model

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+
+  has_many :foods, foreign_key: 'user_id'
+  has_many :recipes, foreign_key: 'user_id'
+
+  def admin?
+    role == 'admin'
+  end
+end
