d13d75e4bbfa5125eb57f3aba79b8fec2a741551
Fene-87 <fenekayas87@gmail.com>
2023-05-23 00:31:04 +0300

    Configure signup and login for app with devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_22_194725) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_22_212115) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email", default: "", null: false
     t.string "role"
     t.datetime "created_at", null: false
@@ -24,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_22_194725) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
