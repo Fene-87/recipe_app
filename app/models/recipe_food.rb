89953fea18db09ab86f0efab0172c56f088df698
Ermi <manermidem@gmail.com>
2023-05-22 19:01:23 +0300

    Edit recipe food model for the app

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,2 +1,6 @@
 class RecipeFood < ApplicationRecord
+    belongs_to :recipe
+    belongs_to :foods
+
+    validates :quantity, presence: { message: "Quantity is required" } , numericality: { only_integer: true, greater__than_or_equal_to: 0, message: "Quantity must be a positive integer"}
 end
