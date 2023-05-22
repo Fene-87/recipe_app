e13fc042893105c7a4d8894fb9b0b2dfd6329456
Ermi <manermidem@gmail.com>
2023-05-22 20:59:52 +0300

    Fix linters error

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,6 +1,7 @@
 class RecipeFood < ApplicationRecord
-    belongs_to :recipe
-    belongs_to :foods
+  belongs_to :recipe
+  belongs_to :foods
 
-    validates :quantity, presence: { message: "Quantity is required" } , numericality: { only_integer: true, greater__than_or_equal_to: 0, message: "Quantity must be a positive integer"}
+  validates :quantity, presence: { message: 'Quantity is required' },
+                       numericality: { only_integer: true, greater__than_or_equal_to: 0, message: 'Quantity must be a positive integer' }
 end
