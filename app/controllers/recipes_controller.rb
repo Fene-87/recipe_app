052fe264496996bdbe731281e30fd2b4ab707733
Ermi <manermidem@gmail.com>
2023-05-23 17:52:22 +0300

    Add a method for the recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,17 @@
 class RecipesController < ApplicationController
+    def index
+        # @recipes = Recipe.all
+        recipes = 'List of recipes'
+    end
+
+    def show
+        # @recipe = Recipe.find(params[:id])
+        recipe = 'Recipe details'
+    end
+
+    def destroy
+        @recipe = Recipe.find(params[:id], current_user)
+        @recipe.destroy
+        redirect_to recipes_path
+    end
 end
