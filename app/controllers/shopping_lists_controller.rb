d863475af6c31ec7252fd503b39e0980fead72f4
Ermi <manermidem@gmail.com>
2023-05-25 17:02:50 +0300

    Update shopping lists

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,25 +1,35 @@
 class ShoppingListsController < ApplicationController
-  def index
-    @recipe = Recipe.find(params[:recipe_id])
-    @shopping_lists = create_shopping_list(@recipe)
-  end
-
-  private
-
-  def create_shopping_list(recipe)
-    shopping_list = {}
-
-    recipe.recipe_foods.each do |recipe_food|
-      food = recipe_food.food
-      quantity = recipe_food.quantity
-
-      if shopping_list[food.name]
-        shopping_list[food.name] += " #{quantity}"
-      else
-        shopping_list[food.name] = quantity
-      end
+    def index
+      @recipe = Recipe.find(params[:recipe_id])
+      @shopping_list = create_shopping_list(@recipe)
+      @missing_foods = find_missing_foods(@shopping_list)
     end
-
-    shopping_list
+  
+    private
+  
+    def create_shopping_list(recipe)
+      shopping_list = {}
+  
+      recipe.recipe_foods.each do |recipe_food|
+        food = recipe_food.food
+        quantity = recipe_food.quantity
+  
+        if shopping_list[food.name]
+          shopping_list[food.name] += " #{quantity}"
+        else
+          shopping_list[food.name] = quantity
+        end
+      end
+  
+      shopping_list
+    end
+  
+    def find_missing_foods(shopping_list)
+      all_foods = Food.pluck(:name)
+      shopping_foods = shopping_list.keys
+      missing_foods = all_foods - shopping_foods
+  
+      missing_foods
+    end
   end
-end
+  
