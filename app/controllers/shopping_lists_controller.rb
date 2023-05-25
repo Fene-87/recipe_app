8f1d2c1dfef14e5214a3736bab02805362e1c645
Ermi <manermidem@gmail.com>
2023-05-25 16:45:01 +0300

    Add controller for the shopping lists

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,2 +1,26 @@
 class ShoppingListsController < ApplicationController
+
+    def index
+        @recipe = Recipe.find(params[:recipe_id])
+        @shopping_lists = create_shopping_list(@recipe)
+    end
+
+    private
+
+    def create_shopping_list(recipe)
+        shopping_list = {}
+    
+        recipe.recipe_foods.each do |recipe_food|
+          food = recipe_food.food
+          quantity = recipe_food.quantity
+    
+          if shopping_list[food.name]
+            shopping_list[food.name] += " #{quantity}"
+          else
+            shopping_list[food.name] = quantity
+          end
+        end
+    
+        shopping_list
+      end
 end
