a108f6ad6ff4a6539090ba9dd7c4a9424df3ee33
Ermi <manermidem@gmail.com>
2023-05-26 20:27:44 +0300

    Add a test for the recipe_food view

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -46,4 +46,28 @@ RSpec.describe RecipeFood, type: :model do
       expect(recipe_food).to_not be_valid
     end
   end
+
+  describe 'View' do
+    it 'should have a name' do
+      expect(recipe.name).to eq('Recipe Name')
+    end
+
+    it 'should have a description' do
+      expect(recipe.description).to eq('Recipe Description')
+    end
+
+    it 'should have a public' do
+      expect(recipe.public).to eq(true)
+    end
+
+    it 'should have quantity' do
+      expect(recipe_food.quantity).to eq('1')
+    end
+
+    it 'should have a recipe_id' do
+      expect(recipe_food.recipe_id).to eq(recipe.id)
+    end
+
+  end
+
 end
