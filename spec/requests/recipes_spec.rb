9c5aaef171f05fb8a7ff413948bca6957319c98f
Ermi <manermidem@gmail.com>
2023-05-26 13:22:11 +0300

    Linter fix

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Recipes', type: :request do
   before(:each) do
     @user = User.create(name: 'User Name', email: 'manermidem@gmail.com')
     @recipe = Recipe.create(name: 'Recipe Name', description: 'Recipe Description', public: true,
-                            preparation_time: '10',cooking_time: '10', user_id: @user.id)
+                            preparation_time: '10', cooking_time: '10', user_id: @user.id)
     @food = Food.create(name: 'Food Name', user_id: @user.id, measurement_unit: 'KG', price: 10, quantity: 10)
     @recipe_food = RecipeFood.create(recipe_id: @recipe.id, food_id: @food.id, quantity: 1)
 
